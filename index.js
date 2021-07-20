a4d50e5b8d2cdb55c19380226b630c3e52d80265
ShenQi1996 <qisam1989@gmail.com>
2021-07-20 14:36:46 -0400

    add express routes 7/20/2021

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
 
 mongoose
-  .connect(db, { useUnifiedTopology: true,
-                useNewUrlParser: true,
-                useCreateIndex: true })
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
