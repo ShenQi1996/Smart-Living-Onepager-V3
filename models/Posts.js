bfda6e9a49592a41708a2dfb5f4106bcc25f5b3a
ShenQi1996 <qisam1989@gmail.com>
2021-07-29 16:06:53 -0400

    createed post schema

---
+++ models/Posts.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = User = mongoose.model("Post", PostSchema);
