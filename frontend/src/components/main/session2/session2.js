59c9ae5d26b9b2d806655766bed8761b6bd901d8
ShenQi1996 <qisam1989@gmail.com>
2021-09-24 14:13:26 -0400

    session2 inner box

--- frontend/src/components/main/session2/session2.js
+++ frontend/src/components/main/session2/session2.js
@@ -3,9 +3,35 @@ import React from "react";
 const Session2 = props => {
   return (
     <div className="Session2_outter">
-      <div className="Session2_box1"></div>
-      <div className="Session2_box2"></div>
-      <div className="Session2_box3"></div>
+      <div className="Session2_box1">
+        <div className="Session2_box1_inner1">
+          <h1>01</h1>
+        </div>
+        <div className="Session2_box1_inner2">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's
+          </p>
+        </div>
+        <div className="Session2_box1_inner3"></div>
+        <div className="Session2_box1_inner4"></div>
+      </div>
+      <div className="Session2_box2">
+        <div>
+          <h1>02</h1>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="Session2_box3">
+        <div>
+          <h1>03</h1>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
